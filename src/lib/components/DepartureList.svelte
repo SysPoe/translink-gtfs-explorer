3f7f7efa970870bef5db1bc9df7b6d7034d072e7
SysPoe <panddplayz@outlook.com>
2025-07-02 21:01:15 +1000

    Revert "Enhance departure list with realtime trip and stop time updates"
    This reverts commit ded2c0d3d053bf5bbe29300fdd6081d3ed840ac1.

--- src/lib/components/DepartureList.svelte
+++ src/lib/components/DepartureList.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	
+	import { routeAttributes } from 'gtfs/models';
 
 	export let stopTimes: any[];
 	export let trips: any;
@@ -7,33 +7,6 @@
 	export let stops: any;
 	export let expressInfos: any;
 	export let runGurus: any;
-
-	function formatTime(time: string, delay: number | undefined) {
-		if (delay === undefined) {
-			return time.slice(0, 5);
-		}
-
-		const [hours, minutes, seconds] = time.split(':').map(Number);
-		const totalSeconds = hours * 3600 + minutes * 60 + seconds + delay;
-
-		const newHours = Math.floor(totalSeconds / 3600);
-		const newMinutes = Math.floor((totalSeconds % 3600) / 60);
-
-		const formattedHours = String(newHours).padStart(2, '0');
-		const formattedMinutes = String(newMinutes).padStart(2, '0');
-
-		const delayMinutes = Math.round(delay / 60);
-		let delayString = '';
-		if (delayMinutes === 0) {
-			delayString = '(on time)';
-		} else if (delayMinutes > 0) {
-			delayString = `(${delayMinutes}m late)`;
-		} else {
-			delayString = `(${-delayMinutes}m early)`;
-		}
-
-		return `${formattedHours}:${formattedMinutes} ${delayString}`;
-	}
 </script>
 
 <ul>
@@ -43,22 +16,10 @@
 		{@const stop = stops[dep.stop_id || '']}
 		<li>
 			<hr />
-			{#if dep.schedule_relationship === 'SKIPPED'}
-				<del>{formatTime(dep.arrival_time || '00:00:00', dep.arrival_delay)} p{stop.platform_code} -
-				<a href="/trip/{trip.trip_id}">{trip.trip_id.slice(-4)}</a>
-				{route.route_short_name}
-				{(trip.trip_headsign || 'unknown').replace('station', '').trim()}</del> (cancelled)
-			{:else if dep.schedule_relationship === 'NEW' || dep.schedule_relationship === 'REPLACEMENT'}
-				{formatTime(dep.arrival_time || '00:00:00', dep.arrival_delay)} p{stop.platform_code} -
-				<a href="/trip/{trip.trip_id}">{trip.trip_id.slice(-4)}</a>
-				{route.route_short_name}
-				{(trip.trip_headsign || 'unknown').replace('station', '').trim()} (added)
-			{:else}
-				{formatTime(dep.arrival_time || '00:00:00', dep.arrival_delay)} p{stop.platform_code} -
-				<a href="/trip/{trip.trip_id}">{trip.trip_id.slice(-4)}</a>
-				{route.route_short_name}
-				{(trip.trip_headsign || 'unknown').replace('station', '').trim()}
-			{/if}
+			{(dep.arrival_time || '??:??:??').slice(0, 5)} p{stop.platform_code} -
+			<a href="/trip/{trip.trip_id}">{trip.trip_id.slice(-4)}</a>
+			{route.route_short_name}
+			{(trip.trip_headsign || 'unknown').replace('station', '').trim()}
 			{#if dep.drop_off_type == 1}
 				(No Drop Off)
 			{/if}
