93d01f000a74a62873a07f4f6ab09e9683ed8672
SysPoe <panddplayz@outlook.com>
2025-07-11 20:21:58 +1000

    Change live departure refresh behaviour to reload page every 15 seconds.
    Yes this is suboptimal

--- src/routes/stations/[station]/departures/live/+page.svelte
+++ src/routes/stations/[station]/departures/live/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { invalidate } from '$app/navigation';
 	import DepartureList from '$lib/components/DepartureList.svelte';
+	import { onMount } from 'svelte';
 
 	let { data } = $props();
 	let info = data.stationInfo;
@@ -12,22 +11,20 @@
 		return value;
 	};
 
-	onMount(() => {
-		const interval = setInterval(() => {
-			invalidate('/stations/' + info.stop_id + '/departures/live');
-		}, 30000);
-
-		return () => clearInterval(interval);
-	});
-
 	function switchToNormal() {
 		let id = info.stop_id;
 		const now = new Date();
 		const date = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}`;
-		const start = "00:00:00";
-		const end = "28:00:00";
+		const start = '00:00:00';
+		const end = '28:00:00';
 		location.href = `/stations/${id}/departures/${date}/${start}/${end}`;
 	}
+
+	onMount(() => {
+		setTimeout(() => {
+			location.reload();
+		}, 15000);
+	});
 </script>
 
 <h1>
