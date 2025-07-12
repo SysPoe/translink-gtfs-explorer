c8ec3c6993a3fb0eda82d4172d7b14fecb1ea8c1
SysPoe <panddplayz@outlook.com>
2025-07-12 12:28:24 +1000

    Remove unused search page components and server logic

--- src/routes/search/[query]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './[date]/$types';
-import { getRuns } from '$lib/run';
-
-export const load: PageServerLoad = async ({ params }) => {
-	return {
-		query: params.query,
-		runs: await getRuns(params.query)
-	};
-};
